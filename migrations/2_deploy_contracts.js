ab3faf67767ca3f7dc9549f3eb09d77d3ad33d83
Lubin <tanlubin.official@gmail.com>
2019-09-20 08:57:43 +0800

    missed out on commiting the deploy js after adding an input to the Remittance contract constructor.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
+const bigNum = web3.utils.toBN;
+
 // const ConvertLib = artifacts.require("ConvertLib");
 const Remittance = artifacts.require("Remittance");
+const initialFee = bigNum(1e16);
 
 module.exports = function(deployer) {
   // deployer.deploy(ConvertLib);
   // deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(Remittance);
+  deployer.deploy(Remittance, initialFee);
 };
